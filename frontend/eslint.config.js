e3822ca44dde7aaf2d1438ca1c2cfe05357e2394
FerreitoDev <contato.gustavoferreirad.almeida@gmail.com>
2026-08-14 12:12:05 -0300

    chore: estabelece fundação do frontend

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -14,6 +14,12 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      {
+        files: ['src/components/ui/**/*.{ts,tsx}'],
+        rules: {
+          'react-refresh/only-export-components': 'off',
+      },
+},
     ],
     languageOptions: {
       globals: globals.browser,
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>CHAMAdos</div>
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">CHAMAdos</h1>
+    </div>
+  )
 }
 
 export default App
