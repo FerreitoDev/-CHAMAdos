8a4afef82a824eba20509549fa9d5c54d126f833
FerreitoDev <contato.gustavoferreirad.almeida@gmail.com>
2026-09-03 10:52:36 -0300

    build(backend): adiciona class-validator e global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove propriedades desnecessárias
+      forbidNonWhitelisted: true, // Rejeita payloads com propriedades não esperadas
+      transform: true, // Transforma parâmetros tipados (ex: strings para números)
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((error) => {
